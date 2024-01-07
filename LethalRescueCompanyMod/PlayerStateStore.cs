8f4651b5749b9dbfab789ca08e85d88bc38fc32d
Administrator <root>
2024-01-07 16:11:12 +0200

    enabled c# 8

---
+++ LethalRescueCompanyMod/PlayerStateStore.cs
@@ -0,0 +1,15 @@
+﻿using GameNetcodeStuff;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LethalRescueCompanyMod
+{
+    public static class PlayerStateStore
+    {
+        public static PlayerControllerB? playerControllerB = GameNetworkManager.Instance.localPlayerController;
+
+    }
+}
