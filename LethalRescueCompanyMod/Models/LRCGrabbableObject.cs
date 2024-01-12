4f30d5bc852f7ca054436c53c5bce39bde06c590
Kegan Holtzhausen <kegan.holtzhausen@kindredgroup.com>
2024-01-12 23:05:44 +0100

    Networking for grabbables

--- LethalRescueCompanyMod/Models/LRCGrabbableObject.cs
+++ LethalRescueCompanyMod/Models/LRCGrabbableObject.cs
@@ -1,4 +1,7 @@
 ﻿using BepInEx.Logging;
+using LethalRescueCompanyMod.NetworkBehaviors;
+using System;
+using Unity.Netcode;
 
 namespace LethalRescueCompanyMod.Models
 {
@@ -8,11 +11,42 @@ namespace LethalRescueCompanyMod.Models
     public class LRCGrabbableObject : GrabbableObject
     {
 
-        static internal ManualLogSource log = BepInEx.Logging.Logger.CreateLogSource("LethalRescueCompanyPlugin.NetworkBehaviors.LRCGrabbableObject");
+        internal ManualLogSource log = BepInEx.Logging.Logger.CreateLogSource("LethalRescueCompanyPlugin.NetworkBehaviors.LRCGrabbableObject");
+
 
         public override void InteractItem()
         {
             log.LogInfo("interact item");
+        }
+
+
+        public override void OnNetworkSpawn()
+        {
+            log.LogInfo("network spawn");
+
+            RescueCompanyController.ServerEvent += ServerEventHandler;
+            RescueCompanyController.ClientEvent += ClientEventHandler;
+
+            base.OnNetworkSpawn();
+        }
+
+        public override void OnNetworkDespawn()
+        {
+
+            RescueCompanyController.ServerEvent -= ServerEventHandler;
+            RescueCompanyController.ClientEvent -= ClientEventHandler;
+
+            base.OnNetworkDespawn();
+        }
+
+        private void ClientEventHandler(Event @event)
+        {
+            log.LogInfo($"test: clientevent: {@event}");
+        }
+
+        private void ServerEventHandler(Event @event)
+        {
+            log.LogInfo($"test: serverevent: {@event}");
         }
     }
 }
